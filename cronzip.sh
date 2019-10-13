62a3206f94974c49c603cb734b805a8fc4b9bb59
ylakade <56476665+ylakade@users.noreply.github.com>
2019-10-13 15:20:27 +0530

    Add files via upload

---
+++ cronzip.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Usage: ./cal.sh file
+# Usage: bash cal.sh 
+# let varible
+
+cd /home/student/kiran/scripts/knowit
+
+zip a.zip *.json
+
+mv a.zip /home/student/kiran/scripts/zip
+
+
+
+
+
